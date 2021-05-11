1e717b63506697445c170892df00f3767e6f98b5
LiquiNaut <boetfly@gmail.com>
2021-05-11 15:05:28 +0200

    oprava index.php

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
 
+?>
 
-prdel
+<!doctype html>
+
+<html lang="sk">
+<head>
+    <meta charset="utf-8">
+
+    <title>Skuskove_zadanie</title>
+    <meta name="description" content="Skuskove_zadanie">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+<script src="js/scripts.js"></script>
+</body>
+</html>
