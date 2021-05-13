228194cafdc757e9fc7a51ea200e57832d5ed69b
cernan <lalohlupy@gmail.com>
2021-05-13 17:11:02 +0200

    1.1.6

--- vytvaranie_testov/index_cernan.php
+++ vytvaranie_testov/index_cernan.php
@@ -16,9 +16,11 @@
 </head>
 <body class="container">
 
-<ol id="testList"></ol>
 
 
+<div id="myDiv">
+    <ol id="testList"></ol>
+</div>
 <label for="testName">Nazov testu:</label>
 <textarea id="testName" name="testName" rows="1" cols="25"></textarea>
 <ul>
@@ -39,7 +41,9 @@
 <button onclick="myFunction2()">get all questions</button>
 <button id="btn2">Show HTML</button>
 
+<div id="testDiv">
 
+</div>
 
 
 
@@ -50,46 +54,87 @@
     var index = 0;
 
 function myFunction() {
-    var idName = "question"
-    var node = document.createElement("LI");                //pridaj list
-    node.setAttribute("id" , idName+index);
-    index++;
-    var text = document.getElementById("testName").value;      //pridaj nazov
-    var textnode = document.createTextNode(text);
-    node.appendChild(textnode);
+    var questionId = "question";
+    var divId = "div"+index;
+    var divNode = document.createElement("DIV");
+    divNode.setAttribute("id" , divId);
+    document.getElementById("testList").appendChild(divNode);
 
+    if(document.getElementById(divId) != null){
 
-    var check1 = document.getElementById("multipleAnswers").checked;
-    var check2 = document.getElementById("shortAnswer").checked;
-    if(check1 == true){
-        text = "- vyber z moznosti";
-        textnode = document.createTextNode(text);
+        var node = document.createElement("LI");                //pridaj list
+        node.setAttribute("id" , questionId+index);
+        index++;
+        var text = document.getElementById("testName").value;      //pridaj nazov
+        var textnode = document.createTextNode(text);
         node.appendChild(textnode);
-    }
-    else if(check2 == true){
-        var textarea = document.createElement("textarea");
-        text = "- kratka slovna odpoved";
-        textnode = document.createTextNode(text);
-        node.appendChild(textnode);
-    }
-    else {
-        text = "- parovanie otazok";
-        textnode = document.createTextNode(text);
-        node.appendChild(textnode);
-    }
-    //alert(text);
-    document.getElementById("testList").appendChild(node);
-    document.getElementById("testList").appendChild(textarea);
 
 
-};
+        var check1 = document.getElementById("multipleAnswers").checked;
+        var check2 = document.getElementById("shortAnswer").checked;
+        if(check1 == true){
+            text = "- vyber z moznosti";
+            textnode = document.createTextNode(text);
+            node.appendChild(textnode);
+        }
+        else if(check2 == true){
+            var textarea = document.createElement("textarea");
+            text = "- kratka slovna odpoved";
+            textnode = document.createTextNode(text);
+            node.appendChild(textnode);
+        }
+        else {
+            text = "- parovanie otazok";
+            textnode = document.createTextNode(text);
+            node.appendChild(textnode);
+        }
+        alert(divId);
+        tempId = "#"+divId;
+        $(tempId).append(node);
+        document.getElementById(divId).appendChild(node);
+        document.getElementById(divId).appendChild(textarea);
+    }
+
+
+
+}
 
 function myFunction2(){
-    $("#myDiv :input").hide(); // :input matches all input elements, including selects
-};
+    var temp = $("#testList").html();
+    var json = JSON.stringify(temp);
+    alert(json);
+
+}
 
 $("#btn2").click(function(){
-    alert("HTML: " + $("#testList").html());
+    var jsonObject = $("#testList").html();
+
+    $.ajax({
+        type : "POST",
+        url : "testsIO.php",
+        dataType : 'text',
+        data : {
+            json : jsonObject
+        },
+        success: function (data){
+            alert(data);
+        },
+        error: function (xhr ,request , error){
+            console.log(arguments);
+            var err = eval("(" + xhr.responseText + ")");
+            alert(err.Message);
+        }
+
+    });
+
+    //var myJSON = JSON.stringify(temp);
+    //localStorage.setItem("testJSON", myJSON);
+
+    //text = localStorage.getItem("testJSON");
+    //obj = JSON.parse(temp);
+    //alert(temp);
+    //$("#testDiv").append(temp);
+    //document.getElementById("testDiv").appendChild(temp);
 });
 
 
---
+++ vytvaranie_testov/testsIO.php
@@ -0,0 +1,13 @@
+<?php
+
+
+    $json = $_POST['json'];
+    return var_dump($json);
+   /*$info = json_encode($json);
+
+    $file = fopen('new_map_data.txt','w+');
+    fwrite($file, $info);
+    fclose($file);
+    return 1;*/
+
+?>
