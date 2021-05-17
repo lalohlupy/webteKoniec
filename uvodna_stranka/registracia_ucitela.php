436d2443e8e04655d422905a40f5a05503a4a4c7
LiquiNaut <boetfly@gmail.com>
2021-05-17 22:38:02 +0200

    nove css pre ucitel registration

--- uvodna_stranka/registracia_ucitela.php
+++ uvodna_stranka/registracia_ucitela.php
@@ -6,31 +6,32 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="../css/style.css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../css/student_login_css.css">
     <title>Registracia</title>
 </head>
-<body style="background-color: #aaaabb;">
+<body>
     <div class="container">
-        <div class="center">
+        <div class="wrapper fadeInDown">
+            <div id="formContent">
             <form method="post">
-                <label for="meno_ucitel">Meno:</label><br>
-                <input type="text" id="meno_ucitel" name="meno_ucitel" required><br>
+                <label for="meno_ucitel"><b>Meno:</b></label><br>
+                <input type="text" id="meno_ucitel" class="fadeIn first" name="meno_ucitel" placeholder="meno" required><br>
 
-                <label for="priezvisko_ucitel">Priezvisko:</label><br>
-                <input type="text" id="priezvisko_ucitel" name="priezvisko_ucitel" required><br>
+                <label for="priezvisko_ucitel"><b>Priezvisko:</b></label><br>
+                <input type="text" id="priezvisko_ucitel" class="fadeIn second" name="priezvisko_ucitel" placeholder="priezvisko" required><br>
 
-                <label for="id">Id:</label><br>
-                <input type="text" id="id" name="id" required><br>
+                <label for="id"><b>AIS Id:</b></label><br>
+                <input type="text" id="id" class="fadeIn third" name="id" placeholder="AIS_id" required><br>
 
                 <label for="password">Heslo:</label><br>
-                <input type="password" id="password" name="password" required><br><br>
+                <input type="password" id="password" class="fadeIn fourth" name="password" placeholder="password" required><br><br>
 
-                <input type="submit" class="btn btn-primary" value="Potvrdit"><br><br>
+                <input type="submit" class="btn btn-primary fadeIn fifth" value="Potvrdit"><br><br>
             </form>
-
+            </div>
             <?php
                 require_once '../vytvaranie_testov/Controller.php';
                 $controller = new Controller();
