6e4ba5ea2c957411bf103062af7965939974eb90
LiquiNaut <boetfly@gmail.com>
2021-05-17 23:29:52 +0200

    nove css pre uvodnu stranku

--- uvodna_stranka/index.php
+++ uvodna_stranka/index.php
@@ -6,20 +6,20 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-<!--    <link rel="stylesheet" href="../css/style.css">-->
-    <title>Prihlasenie</title>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../css/student_login_css.css">
+    <title>Typ Prihlasenia</title>
 </head>
 </html>
-<body style="background-color: #aaaabb;">
-    <div style="background-color: whitesmoke; margin-top: 200px; border-radius: 15px; width: 50%; margin-left: 400px; border: solid 2px ">
-        <div class="container">
-            <div class="center" style=" display: flex;flex-direction: column;align-items:center; padding: 5px; width: 70%">
-            <h1>Prihlásiť sa ako: </h1>
-            <a class="size_a" href="student.php" style="border: solid 2px; height: 50px; margin: 10px; padding: 10px;border-radius: 10px; background-color: aqua; font-size: larger">Študent</a>
-            <a class="size_a" href="ucitel.php" style="border: solid 2px; height: 50px; margin: 10px; padding: 10px;border-radius: 10px; background-color: aqua; font-size: larger" >Učiteľ</a>
+<body>
+    <div class="container">
+        <div class="wrapper fadeInDown">
+            <div id="formContent">
+                <h1>Prihlásiť sa ako: </h1>
+                <a class="fadeIn first" id="student" href="student.php" style="color: #92badd;text-decoration: none;font-weight: 400; font-size: 150%; display: flex;flex-direction: column;align-items:center; "><b>Študent</b></a>
+                <a class="fadeIn second" id="ucitel" href="ucitel.php" style="color: #92badd;text-decoration: none;font-weight: 400; font-size: 150%; display: flex; flex-direction: column;align-items:center; "><b>Učiteľ</b></a>
             </div>
         </div>
     </div>
