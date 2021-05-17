4616b8585e49e22d01ed93ff9d5a0059c889b4cd
LiquiNaut <boetfly@gmail.com>
2021-05-17 02:15:11 +0200

    zmena headeru()

--- uvodna_stranka/ucitel.php
+++ uvodna_stranka/ucitel.php
@@ -40,7 +40,7 @@
                         $result = $veta->fetchColumn();
                         $heslo = $_POST['heslo_ucitel'];
                         if (password_verify($heslo, $result)) {
-                            header("Location: ../vytvaranie_testov/index_cernan.php");
+                            header("Location: ../pohlad_ucitel/index.php");
                         }
                         else if(!password_verify($heslo, $result)){
                             echo "Zadané heslo je nesprávne!";
